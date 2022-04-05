36f76ddb18ac4e45654ee096dbb35ce8fd98ceb6
fleck-sara <sfleck@udel.edu>
2022-04-05 15:12:51 -0400

    added Sara Fleck

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <h6>Welcome to the scorer web page!</h6>
+            <h6>Welcome to the scorer web page! Sara Fleck</h6>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
